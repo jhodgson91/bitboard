f44c8700877435073febd8abaceaa8eaba0f7e13
jhodgson91 <jhodgson71@gmail.com>
2019-03-02 01:10:25 +0000

    Initial commit - lots of tests need doing...

---
+++ src/lib.rs
@@ -0,0 +1,309 @@
+#![feature(allocator_api)]
+
+extern crate num;
+extern crate typenum;
+
+use num::PrimInt;
+use std::alloc::{Alloc, Layout, System};
+use std::fmt::{Binary, Debug, Display, Formatter};
+use std::marker::PhantomData;
+use std::mem;
+use std::ops::{BitAndAssign, BitOrAssign, Shl, ShlAssign, Shr, ShrAssign};
+use std::ptr::NonNull;
+use typenum::*;
+
+pub trait PrimUInt:
+    PrimInt
+    + num::Unsigned
+    + num::Zero
+    + BitAndAssign
+    + BitOrAssign
+    + Display
+    + Binary
+    + Shl
+    + Shr
+    + ShlAssign
+    + ShrAssign
+{
+}
+
+impl PrimUInt for u8 {}
+impl PrimUInt for u16 {}
+impl PrimUInt for u64 {}
+impl PrimUInt for u32 {}
+impl PrimUInt for u128 {}
+
+pub struct BitBoard<N: Unsigned, R: PrimUInt = u64> {
+    ptr: *mut R,
+    _typenum: PhantomData<N>,
+}
+
+impl<N: Unsigned, R: PrimUInt> BitBoard<N, R> {
+    pub fn set(&mut self, x: usize, y: usize) {
+        if Self::in_bounds(x, y) {
+            let (offset, bit_pos) = Self::coords_to_offset_and_pos(x, y);
+            unsafe { (*self.ptr.offset(offset) |= bit_pos) };
+        }
+    }
+
+    pub fn unset(&mut self, x: usize, y: usize) {
+        if Self::in_bounds(x, y) {
+            let (offset, bit_pos) = Self::coords_to_offset_and_pos(x, y);
+            unsafe { (*self.ptr.offset(offset) &= !bit_pos) };
+        }
+    }
+
+    pub fn is_set(&self, x: usize, y: usize) -> bool {
+        if Self::in_bounds(x, y) {
+            let (offset, bit_pos) = Self::coords_to_offset_and_pos(x, y);
+            return unsafe { (*self.ptr.offset(offset) & bit_pos) != R::zero() };
+        }
+        false
+    }
+
+    fn in_bounds(x: usize, y: usize) -> bool {
+        x < N::to_usize() && y < N::to_usize()
+    }
+
+    fn coords_to_offset_and_pos(x: usize, y: usize) -> (isize, R) {
+        let pos = x + y * N::to_usize();
+        let byte_offset = pos / Self::alignment_bits();
+        let bit_pos: usize = 1 << (pos % Self::alignment_bits());
+
+        // TODO: Unwrap here
+        (byte_offset as isize, R::from(bit_pos).unwrap())
+    }
+
+    #[inline(always)]
+    fn board_size() -> usize {
+        // This could technically be compile-time as well, but the trait bounds were a goddam nightmare...
+        N::to_usize().pow(2)
+    }
+
+    /// Total number of bits necessary to represent this BitBoard
+    #[inline(always)]
+    fn total_bits() -> usize {
+        let remainder = Self::board_size() / Self::alignment_bits();
+        match remainder {
+            0 => Self::board_size(),
+            _ => Self::board_size() + Self::alignment_bits() - remainder,
+        }
+    }
+
+    /// Total number of bytes necessary to represent this BitBoard
+    #[inline(always)]
+    pub fn total_bytes() -> usize {
+        (Self::total_bits() as f32 / 8.0).ceil() as usize
+    }
+
+    #[inline(always)]
+    pub fn total_used_bytes() -> usize {
+        (Self::total_bytes() as f32 / Self::alignment() as f32).ceil() as usize
+    }
+
+    #[inline(always)]
+    fn alignment() -> usize {
+        mem::align_of::<R>()
+    }
+
+    #[inline(always)]
+    fn alignment_bits() -> usize {
+        Self::alignment() * 8
+    }
+
+    unsafe fn block_at(&mut self, i: isize) -> *mut R {
+        self.ptr.offset(i)
+    }
+
+    // Number of bis in a single block
+    #[inline(always)]
+    fn block_size_bits() -> usize {
+        mem::size_of::<R>() * 8
+    }
+
+    fn layout() -> Layout {
+        Layout::from_size_align(Self::total_bytes(), Self::alignment()).unwrap()
+    }
+}
+
+impl<N: Unsigned, R: PrimUInt> Default for BitBoard<N, R> {
+    fn default() -> Self {
+        let layout = Self::layout();
+        let ptr;
+
+        unsafe {
+            match System.alloc_zeroed(layout) {
+                Ok(p) => ptr = p.as_ptr() as *mut R,
+                Err(e) => panic!("Failed to allocate bitboard! {}", e),
+            }
+        };
+
+        BitBoard {
+            ptr,
+            _typenum: PhantomData,
+        }
+    }
+}
+
+impl<N: Unsigned, R: PrimUInt> Drop for BitBoard<N, R> {
+    fn drop(&mut self) {
+        let layout = Self::layout();
+        unsafe { System.dealloc(NonNull::new(self.ptr as *mut _).unwrap(), layout) }
+    }
+}
+
+impl<N: Unsigned, R: PrimUInt> Clone for BitBoard<N, R> {
+    fn clone(&self) -> Self {
+        let result: BitBoard<N, R> = BitBoard::default();
+        unsafe {
+            std::ptr::copy(self.ptr as *const R, result.ptr, Self::total_used_bytes());
+        }
+        result
+    }
+}
+
+impl<N: Unsigned, R: PrimUInt> Shl<usize> for BitBoard<N, R> {
+    type Output = Self;
+
+    fn shl(mut self, mut rhs: usize) -> Self {
+        let mut lost: R;
+        let mut prev_lost: R;
+
+        let mut current: *mut R;
+
+        unsafe {
+            while rhs > 0 {
+                prev_lost = R::zero();
+
+                let to_shift = std::cmp::min(Self::block_size_bits() - 1, rhs);
+                for i in 0..(Self::total_used_bytes() as isize) {
+                    current = self.block_at(i);
+
+                    // lost bits are either everything in
+                    // this block if shift is larger than bit
+                    // size or the reverse of the shift
+                    lost = if to_shift < Self::block_size_bits() {
+                        *current >> (Self::block_size_bits() - to_shift)
+                    } else {
+                        *current
+                    };
+
+                    *current = *current << to_shift;
+
+                    // Set any bits that were lost from the previous block
+                    *current |= prev_lost;
+
+                    prev_lost = lost;
+                }
+
+                rhs -= to_shift;
+            }
+        }
+        self.clone()
+    }
+}
+
+impl<N: Unsigned, R: PrimUInt> Shr<usize> for BitBoard<N, R> {
+    type Output = Self;
+
+    fn shr(mut self, mut rhs: usize) -> Self {
+        let mut lost: R;
+        let mut prev_lost: R;
+
+        let mut current: *mut R;
+
+        unsafe {
+            while rhs > 0 {
+                prev_lost = R::zero();
+
+                let to_shift = std::cmp::min(Self::block_size_bits() - 1, rhs);
+
+                for i in 0..=(Self::total_used_bytes() as isize) {
+                    current = self.block_at(Self::total_used_bytes() as isize - i);
+
+                    lost = if to_shift < Self::block_size_bits() {
+                        *current << (Self::block_size_bits() - to_shift)
+                    } else {
+                        *current
+                    };
+
+                    *current = *current >> to_shift;
+                    *current |= prev_lost;
+
+                    prev_lost = lost;
+                }
+
+                rhs -= to_shift;
+            }
+        }
+        self.clone()
+    }
+}
+
+impl<N: Unsigned, R: PrimUInt> Debug for BitBoard<N, R> {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let s = N::to_usize();
+
+        for c in 0..s {
+            for r in 0..s {
+                if self.is_set(r, c) {
+                    write!(f, "1")?;
+                } else {
+                    write!(f, "0")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        write!(f, "")
+    }
+}
+
+impl<N: Unsigned, R: PrimUInt> Display for BitBoard<N, R> {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let s = N::to_usize();
+
+        for c in 0..s {
+            for r in 0..s {
+                if self.is_set(r, s - c - 1) {
+                    write!(f, "1 ")?;
+                } else {
+                    write!(f, "0 ")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        write!(f, "")
+    }
+}
+
+type BitBoard2x2 = BitBoard<U2, u8>;
+type BitBoard3x3 = BitBoard<U3, u16>;
+type BitBoard4x4 = BitBoard<U4, u16>;
+type BitBoard5x5 = BitBoard<U5, u32>;
+type BitBoard6x6 = BitBoard<U6, u64>;
+type BitBoard7x7 = BitBoard<U7, u64>;
+type BitBoard8x8 = BitBoard<U8, u64>;
+type BitBoard9x9 = BitBoard<U9, u128>;
+type BitBoard10x10 = BitBoard<U10, u128>;
+type BitBoard11x11 = BitBoard<U11, u128>;
+type BitBoard12x12 = BitBoard<U12, u128>;
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn it_works() {
+        println!("Size: {}", BitBoard3x3::total_used_bytes());
+        println!("Alignment: {}", BitBoard3x3::alignment());
+        println!("{:?}", BitBoard3x3::layout());
+
+        let mut t = BitBoard3x3::default();
+        t.set(0, 1);
+        println!("{}", t);
+        t = t << 6;
+        println!("{}", t);
+        t = t >> 6;
+        println!("{}", t);
+    }
+}
